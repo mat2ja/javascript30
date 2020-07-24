6a448d0a74c72811edee47f303416fbe440241f2
Matija Osrecki <matija.osrecki@gmail.com>
2020-07-24 11:46:24 +0200

    added css variables :boom:

---
+++ 3-CSS_Variables/app.js
@@ -0,0 +1,13 @@
+const inputs = document.querySelectorAll('.controls input'); // Node-List
+
+function handleUpdate() {
+
+    const suffix = this.dataset.sizing || '';
+
+    // target :root with documentElement (basically html)
+    document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate));
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
