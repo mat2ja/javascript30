64fc915d5f251c7bfc755148e97a4bb3bdf6935b
Matija Osrecki <matija.osrecki@gmail.com>
2020-09-22 14:43:18 +0200

    added filters and rbg 'greenscreen' :fire:

--- 19-Webcam_Fun/scripts.js
+++ 19-Webcam_Fun/scripts.js
@@ -10,7 +10,6 @@ function getVideo() {
     navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         // ask permision for caemera access
         .then(localMediaStream => {
-            console.log(localMediaStream);
             // video.src = window.URL.createObjectURL(localMediaStream);
             video.srcObject = localMediaStream;
             video.play();
@@ -30,6 +29,18 @@ function painToCanvas() {
     // good to return it so we can stop it later 
     return setInterval(() => {
         ctx.drawImage(video, 0, 0, width, height);
+
+        // take the pixels out
+        let pixels = ctx.getImageData(0, 0, width, height);
+
+        // modify pixels
+        // pixels = rgbSplit(pixels)
+        // ctx.globalAlpha = 0.1;
+
+        pixels = greenScreen(pixels);
+
+        // put pixels back
+        ctx.putImageData(pixels, 0, 0)
     }, 16)
 }
 
@@ -46,6 +57,55 @@ function takePhoto() {
     link.innerHTML = `<img src='${data}' alt='Handsome man'>`
     strip.insertBefore(link, strip.firstChild);
 
+}
+
+function redEffect(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i + 0] += 100// RED
+        pixels.data[i + 1] -= 50 // GREEN
+        pixels.data[i + 2] *= 0.5 // BLUE
+        // pixels[i + 3] // APLHA
+    }
+    return pixels;
+}
+
+function rgbSplit(pixels) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
+        pixels.data[i - 150] = pixels.data[i + 0]// RED
+        pixels.data[i + 500] = pixels.data[i + 1] // GREEN
+        pixels.data[i - 150] = pixels.data[i + 2]// BLUE
+        // pixels[i + 3] // APLHA
+    }
+    return pixels;
+}
+
+
+function greenScreen(pixels) {
+    const levels = {};
+
+    document.querySelectorAll('.rgb input').forEach((input) => {
+        levels[input.name] = input.value;
+    });
+
+
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+        red = pixels.data[i + 0];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+        alpha = pixels.data[i + 3];
+
+        if (red >= levels.rmin
+            && green >= levels.gmin
+            && blue >= levels.bmin
+            && red <= levels.rmax
+            && green <= levels.gmax
+            && blue <= levels.bmax) {
+            // take it out!
+            pixels.data[i + 3] = 0;
+        }
+    }
+
+    return pixels;
 }
 
 
